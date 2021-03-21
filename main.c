b3f668fb876d80ea1d2f4f21c274d769530be8b3
Alexander Dahl <post@lespocky.de>
2021-03-21 11:41:18 +0100

    Wait a little more after SIGTERM

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <errno.h>
 #include <signal.h>
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
 #include <unistd.h>
 
 #define DEFAULT_TIMEOUT 30
 
+static bool m_signal_received = false;
+
 void sig_handler( int sig )
 {
     printf( "Received signal %d ...\n", sig );
+    m_signal_received = true;
 }
 
 int main( int argc, char *argv[] )
 {
-    unsigned int timeout = DEFAULT_TIMEOUT;
+    unsigned int time_left, timeout = DEFAULT_TIMEOUT;
     struct sigaction act;
     int ec;
 
@@ -32,6 +37,9 @@ int main( int argc, char *argv[] )
     }
     printf( "Installed signal handler ...\n" );
 
+    /*  report useful information   */
+    printf( "My PID is %u ...\n", getpid() );
+
     /*  do things   */
     if ( argc > 1 )
     {
@@ -40,7 +48,21 @@ int main( int argc, char *argv[] )
 
     printf( "Sleeping for %u seconds now ...\n", timeout );
 
-    sleep( timeout );
+    time_left = sleep( timeout );
+    if ( time_left > 0 )
+    {
+        printf( "Sleep interrupted, %u seconds left ...\n", time_left );
+    }
+
+    if ( m_signal_received )
+    {
+        printf( "Sleeping for another %u seconds ...\n", timeout );
+        time_left = sleep( timeout );
+        if ( time_left > 0 )
+        {
+            printf( "Sleep interrupted, %u seconds left ...\n", time_left );
+        }
+    }
 
     printf( "Bye.\n" );
 
